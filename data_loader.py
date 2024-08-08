c6c53c097aaf2e52f6d921c7147591e7e856a65a
Wojciech Trojanowski <wjf.trojanowski@gmail.com>
2024-08-08 21:29:17 +0200

    Processed data can be saved and loaded. normalize_and_slice() refactor. Added logging.

--- data_loader.py
+++ data_loader.py
@@ -5,85 +5,81 @@ import urllib.request
 import subprocess
 import os
 from scipy.signal import convolve2d
-from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%H:%M')
 
-class tid2013_loader:
-    def __init__(self, download=True, path='databases'):
-        self.url = 'https://www.ponomarenko.info/tid2013/tid2013.rar'
-        self.dir = os.path.join(path, 'tid2013')
-        self.file_path = os.path.join(self.dir, 'tid2013.rar')
+class data_loader:
+    '''Parent class for database-specific loaders.'''
+    def __init__(self):
+        self.catalogue = 'databases'
+        self.dir = None # to be declared within the child class
         self.winrar_path = 'D:\\Programy\\WinRAR\\WinRAR.exe'
         self.sevenzip_path = 'D:\\Programy\\7-Zip\\7z.exe'
-        self.distortion_mapping = {1: 'wn', 2:'wnc', 3:'scn', 4:'mn', 5:'hfn', 6:'in', 7:'qn', 8: 'gblur', 9:'idn', 10: 'jpeg', 11: 'jp2k', 12:'jpegte', 13:'jp2kte'} # According to TID2013 documentation
 
-        os.makedirs(self.dir, exist_ok=True)
-
-        if download:
-            self.download()
-        if self.__patch_files_exist():
-            print("Patch files found. Loading patched data...")
-            data = self.__load_patched_data()
-        else:
-            data = self.prepare_data()
-        self.train, self.val, self.test = self.encode(data)
-
-    def __patch_files_exist(self):
-        # Check if patch files are present in the directory
-        return (os.path.exists(os.path.join(self.dir, 'normalized_distorted_images', 'training', 'patch_training.csv')) and
-                os.path.exists(os.path.join(self.dir, 'normalized_distorted_images', 'validation', 'patch_validation.csv')) and
-                os.path.exists(os.path.join(self.dir, 'normalized_distorted_images', 'test', 'patch_test.csv')))
-
-    def __load_patched_data(self):
-        # Load the existing patched data
-        columns=['image', 'MOS', 'distortion']
-        train_data = pd.read_csv(os.path.join(self.dir, 'normalized_distorted_images', 'training', 'patch_training.csv'))
-        val_data = pd.read_csv(os.path.join(self.dir, 'normalized_distorted_images', 'validation', 'patch_validation.csv'))
-        test_data = pd.read_csv(os.path.join(self.dir, 'normalized_distorted_images', 'test', 'patch_test.csv'))
-        return [train_data, val_data, test_data]
-    
-    def download(self): 
+    def download(self, url, file): 
         try:
-            print(f"Downloading dataset from {self.url}...")
-            urllib.request.urlretrieve(self.url, self.file_path)
-            print(f"Dataset downloaded and saved as '{self.file_path}'")
+            logging.info(f"Downloading dataset from {url}...")
+            urllib.request.urlretrieve(url, file)
+            #print(f"Dataset downloaded and saved as {file}.")
         except Exception as e:
-            print(f"Failed to download dataset: {e}.")
+            logging.error(f"Failed to download dataset: {e}.")
             return False  
 
-        if not self.__extract_with_winrar():
-            self.__extract_with_sevenzip()
+        if not self.extract_with_winrar(file):
+            self.extract_with_7zip(file)
 
-    def __extract_with_winrar(self):
+    def extract_with_winrar(self, file):
         try:
-            print(f"Extracting dataset using WinRAR...")
-            subprocess.run([self.winrar_path, 'x', self.file_path, self.dir], capture_output=True, text=True)
-            print(f"Dataset extracted to '{self.dir}'.")
+            logging.info(f"Extracting dataset using WinRAR...")
+            subprocess.run([self.winrar_path, 'x', file, self.dir], capture_output=True, text=True)
+            logging.info(f"Dataset extracted to '{self.dir}'.")
             return True
         except Exception as e:
-            print(f"Error using WinRAR: {e}")
+            logging.error(f"Error using WinRAR: {e}")
             return False
 
-    def __extract_with_sevenzip(self):
+    def extract_with_7zip(self, file):
         try:
-            print(f"Extracting dataset using 7-Zip...")
-            subprocess.run([self.sevenzip_path, 'x', '-aoa', self.file_path, f'-o{self.dir}'], capture_output=True, text=True)
-            print(f"Dataset extracted to '{self.dir}'.")
+            logging.info(f"Extracting dataset using 7-Zip...")
+            subprocess.run([self.sevenzip_path, 'x', '-aoa', file, f'-o{self.dir}'], capture_output=True, text=True)
+            logging.info(f"Dataset extracted to '{self.dir}'.")
             return True
         except Exception as e:
-            print(f"Error using 7-Zip: {e}.")
+            logging.error(f"Error using 7-Zip: {e}.")
             return False
+        
+    def save_data(self, data, set_type):
+        '''Save the data to disk.'''
+        X, y = data
+        np.save(os.path.join(self.dir, f'X_{set_type}.npy'), X)
+        np.save(os.path.join(self.dir, f'y_{set_type}.npy'), y)
+        logging.info(f"{set_type} data saved successfully.")
 
-    def split_data(self, data1):
-        train_data, test_data = train_test_split(data1, test_size=0.2, random_state=40)
+    def load_data(self, set_type):
+        '''Load the data from disk.'''
+        X = np.load(os.path.join(self.dir, f'X_{set_type}.npy'))
+        y = np.load(os.path.join(self.dir, f'y_{set_type}.npy'))
+        #logging.info(f"{set_type} data loaded successfully.")
+        return X, y
+
+    def data_exist(self):
+        '''Check if patch files are present in the directory.'''
+        return (os.path.exists(os.path.join(self.dir, 'X_train.npy')) and os.path.exists(os.path.join(self.dir, 'y_train.npy')) and
+                os.path.exists(os.path.join(self.dir, 'X_val.npy')) and os.path.exists(os.path.join(self.dir, 'y_val.npy')) and
+                os.path.exists(os.path.join(self.dir, 'X_test.npy')) and os.path.exists(os.path.join(self.dir, 'y_test.npy')))
+
+    def split_data(self, data):
+        train_data, test_data = train_test_split(data, test_size=0.2, random_state=40)
         train_data, val_data = train_test_split(train_data, test_size=0.25, random_state=40)
         return train_data, val_data, test_data
 
-    def __normalize_and_slice(self, train_data, val_data, test_data, patch_size=32, cross=False):
-    
-        def local_normalize(patch, P=3, Q=3, C=1):
+    def normalize_and_slice(self, train_data, val_data, test_data, patch_size=32):
+
+        def normalize_image(patch, P=3, Q=3, C=1):
             kernel = np.ones((P, Q)) / (P * Q)
             patch_mean = convolve2d(patch, kernel, boundary='symm', mode='same')
             patch_sm = convolve2d(np.square(patch), kernel, boundary='symm', mode='same')
@@ -99,75 +95,43 @@ class tid2013_loader:
             for i in range(num_patches_y):
                 for j in range(num_patches_x):
                     patch = image[i*patch_size:(i+1)*patch_size, j*patch_size:(j+1)*patch_size]
-                    patch_path = os.path.join(output_dir_patches, f"{os.path.splitext(image_filename)[0]}_patch_{patch_count}.bmp")
-                    patch_filename = f"{os.path.splitext(image_filename)[0]}_patch_{patch_count}.bmp"
+                    patch_path = os.path.join(output_dir_patches, f"{os.path.splitext(filename)[0]}_patch_{patch_count}.bmp")
+                    patch_filename = f"{os.path.splitext(filename)[0]}_patch_{patch_count}.bmp"
                     cv2.imwrite(patch_path, patch)
-                    # Add patch info to the list
-                    self.patch_info_list.append([patch_filename, mos_value, distortion])
+                    self.patches.append([patch_filename, mos, distortion])
                     patch_count += 1
 
         sets = [(train_data, 'training'), (val_data, 'validation'), (test_data, 'test')]
-        print('Normalizing and slicing images...')
+        dfs = []
+        logging.info('Normalizing images and slicing into patches...')
 
         for (data, name) in sets:
             output_dir_full = os.path.join(self.dir, 'normalized_distorted_images', name, 'full')
             output_dir_patches = os.path.join(self.dir, 'normalized_distorted_images', name, 'patches')
-            norm_file_info_path = os.path.join(self.dir, 'normalized_distorted_images', name, f'norm_{name}.csv')
-            patch_file_info_path = os.path.join(self.dir, 'normalized_distorted_images', name, f'patch_{name}.csv')
             os.makedirs(output_dir_full, exist_ok=True)
             os.makedirs(output_dir_patches, exist_ok=True)
-
-            self.norm_info_list = []
-            self.patch_info_list = []
-
+            self.patches = []
             for row in data.itertuples(index=False):
-                image_filename = row[0]
-                mos_value = row[1]
+                filename = row[0]
+                mos = row[1]
                 distortion = row[2]
-                image_path = f'{self.dir}/distorted_images/{image_filename}'
+                image_path = f'{self.dir}/distorted_images/{filename}'
                 image = cv2.imread(image_path)
-
                 if image is None:
-                    print(f"Failed to load image: {image_filename}")
+                    logging.warning(f"Failed to load image: {filename}")
                     continue
-
-                # Normalize the image
                 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                image_normalized = local_normalize(image_gray)
-                # Save
-                image_filename = f'NORM_{image_filename}'
-                self.norm_info_list.append([image_filename, mos_value, distortion])
-                cv2.imwrite(os.path.join(output_dir_full, image_filename), image_normalized)
-                # Slice to patches
+                image_normalized = normalize_image(image_gray)
+                filename = f'NORM_{filename.lower()}'
+                cv2.imwrite(os.path.join(output_dir_full, filename), image_normalized)
                 slice_image(image_normalized, patch_size)
 
-            norm_info_df = pd.DataFrame(self.norm_info_list, columns=['image', 'MOS', 'distortion'])
-            norm_info_df.to_csv(norm_file_info_path, index=False)
-            print(f"[{name}]: Full image info listed in: {norm_file_info_path}.")
-            patch_info_df = pd.DataFrame(self.patch_info_list, columns=['image', 'MOS', 'distortion'])
-            patch_info_df.to_csv(patch_file_info_path, index=False)
-            print(f"[{name}]: Patch info listed in: {patch_file_info_path}.")
-
-    def prepare_data(self, filter=True):
-        data_path = os.path.join(self.dir, 'mos_with_names.txt')
-        data = pd.read_csv(data_path, header=None, delimiter=' ')
-        data = data.iloc[:, [1, 0]]  # swap column order
-        data.columns = ['image', 'MOS']
-        # Specify distortion type based on filename
-        data['distortion'] = data['image'].apply(lambda x: self.distortion_mapping.get(int(x.split('_')[1]), 'other'))
-        if filter:
-            data = data[data['distortion'].isin(self.distortion_mapping.values())]
-        data.to_csv('databases/tid2013/mos_with_names.csv', index=False)
-
-        train_data, val_data, test_data = self.split_data(data)
-        self.__normalize_and_slice(train_data, val_data, test_data)
-
-        train_data = pd.read_csv(f'{self.dir}/normalized_distorted_images/training/patch_training.csv')
-        val_data = pd.read_csv(f'{self.dir}/normalized_distorted_images/validation/patch_validation.csv')
-        test_data = pd.read_csv(f'{self.dir}/normalized_distorted_images/test/patch_test.csv')
-        return [train_data, val_data, test_data]
+            patches = pd.DataFrame(self.patches, columns=['image', 'MOS', 'distortion'])
+            dfs.append(patches)
+        return dfs
 
     def encode(self, dataframes):
+        '''Encodes distortion labels into one-hot vectors.'''
         for i in range(len(dataframes)):
             dists = dataframes[i]['distortion']
             le = LabelEncoder()
@@ -176,3 +140,80 @@ class tid2013_loader:
             dataframes[i]['distortion_encoded'] = [np.array(one_hot) for one_hot in dists_one_hot]
             dataframes[i] = dataframes[i].drop(['distortion'], axis=1)
         return dataframes
+
+    def map2tf(self, set_type, data):
+        '''
+        Maps data into format excected by TensorFlow: adds chanel dimension and stores data in arrays.
+        dir: path to the dataset directory
+        set_type: 'training', 'validation', 'test' 
+        data: DataFrame with columns: 'image', 'MOS', 'distortion' 
+        '''
+        images_dir = os.path.join(self.dir, 'normalized_distorted_images', set_type, 'patches')
+        X, y = [], []
+        for row in data.itertuples(index=False):
+            filename = row[0]
+            score = row[1]
+            file_path = os.path.join(images_dir, filename)
+            if filename.endswith('.bmp') and os.path.exists(file_path):
+                img = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
+                X.append(img)
+                y.append(score)
+            else:
+                logging.warning(f"File not found: {file_path}")
+        X = np.array(X)
+        y = np.array(y)
+        X = X[..., np.newaxis]
+        return X, y
+# TO DO:
+# error handling: when download=False, download anyway if dataset doesn't exist 
+# error handling: extract when dataset is downloaded but not extracted
+# add error handling in save() and load() methods
+class tid2013_loader(data_loader):
+    def __init__(self, download=True):
+        super().__init__()
+        self.url = 'https://www.ponomarenko.info/tid2013/tid2013.rar'
+        self.dir = os.path.join(self.catalogue, 'tid2013')
+        self.rar_file = os.path.join(self.dir, 'tid2013.rar')
+        self.distortion_mapping = {1: 'wn', 2:'wnc', 3:'scn', 4:'mn', 5:'hfn', 6:'in', 7:'qn', 8: 'gblur', 9:'idn', 10: 'jpeg', 11: 'jp2k', 12:'jpegte', 13:'jp2kte'} # According to TID2013 documentation
+
+        os.makedirs(self.dir, exist_ok=True)
+        if download:
+            self.download(self.url, self.rar_file)
+        elif not os.path.exists(self.rar_file):
+            logging.info("Dataset not found. Downloading...")
+            self.download(self.url, self.rar_file)
+        
+        if self.data_exist():
+            logging.info("Patch files found. Loading patched data...")
+            self.train = self.load_data('train')
+            self.val = self.load_data('val')
+            self.test= self.load_data('test')
+        else:
+            data = self.prepare_data()
+            logging.info('Mapping data to TensorFlow format...')
+            self.train = self.map2tf('training', data[0])
+            self.val = self.map2tf('validation', data[1])
+            self.test = self.map2tf('test', data[2])
+            self.save_data(self.train, 'train')
+            self.save_data(self.val, 'val')
+            self.save_data(self.test, 'test')
+        #self.train, self.val, self.test = self.encode(data)
+
+        logging.info("Data loaded successfully.")
+
+    def prepare_data(self, filter=True):
+        data_path = os.path.join(self.dir, 'mos_with_names.txt')
+        data = pd.read_csv(data_path, header=None, delimiter=' ')
+        data = data.iloc[:, [1, 0]]  # swap column order
+        data.columns = ['image', 'MOS']
+        data['distortion'] = data['image'].apply(lambda x: self.distortion_mapping.get(int(x.split('_')[1]), 'other'))
+        if filter:
+            data = data[data['distortion'].isin(self.distortion_mapping.values())]
+        data.to_csv(os.path.join(self.dir,'mos_with_names.csv'), index=False)
+
+        train_data, val_data, test_data = self.split_data(data)
+        train_data, val_data, test_data = self.normalize_and_slice(train_data, val_data, test_data)
+
+        return [train_data, val_data, test_data]
+    
+#tid2013_loader(download=False)
--- iqa_cnn++.py
+++ iqa_cnn++.py
@@ -8,75 +8,27 @@ import seaborn as sns
 import cv2
 
 from data_loader import tid2013_loader
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Dropout, Input, Flatten
+from tensorflow.keras import layers, models
 
+model = models.Sequential([
+    layers.Input(shape=(32, 32, 1)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(1, activation='linear')
+])
 
+tid2013_loader = tid2013_loader(download=True)
 
-def create_data_generators(base_dir, train_data, val_data, test_data, batch_size):
-    datagen = ImageDataGenerator()
-    train_generator = datagen.flow_from_dataframe(
-        dataframe=train_data,
-        directory=os.path.join(base_dir, 'training/patches/'),
-        x_col='image',
-        y_col=['MOS', 'distortion_encoded'],
-        target_size=(32, 32),
-        batch_size=batch_size,
-        class_mode='multi_output',
-        shuffle=True,
-        seed=42
-    )
-    val_generator = datagen.flow_from_dataframe(
-        dataframe=val_data,
-        directory=os.path.join(base_dir, 'validation/patches/'),
-        x_col='image',
-        y_col=['MOS', 'distortion_encoded'],
-        target_size=(32, 32),
-        batch_size=batch_size,
-        class_mode='multi_output',
-        shuffle=True,
-        seed=42
-    )
-    test_generator = datagen.flow_from_dataframe(
-        dataframe=test_data,
-        directory=os.path.join(base_dir, 'test/patches/'),
-        x_col='image',
-        y_col=['MOS', 'distortion_encoded'],
-        target_size=(32, 32),
-        batch_size=batch_size,
-        class_mode='multi_output',
-        shuffle=False,
-        seed=42
-    )
-    return train_generator, val_generator, test_generator
+X_train, y_train = tid2013_loader.train
+X_val, y_val = tid2013_loader.val
+X_test, y_test = tid2013_loader.test
 
-def define_model(input_shape = (32, 32, 3)):
-    
-    inputs = tf.keras.layers.Input(shape=input_shape)
-    x = tf.keras.layers.Conv2D(8, (3, 3), activation='relu')(inputs)
-    x = tf.keras.layers.MaxPooling2D((2, 2))(x)
-    x = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(inputs)
-    x = tf.keras.layers.MaxPooling2D((2, 2))(x)
-    x = Flatten()(x)
-    x = Dense(128, activation='relu')(x)
-    x = Dense(512, activation='relu')(x)
-
-    mos_output = tf.keras.layers.Dense(1, name='dmos')(x)
-    distortion_output = tf.keras.layers.Dense(13, activation='softmax', name='distortion')(x)
-
-    model = tf.keras.models.Model(inputs=inputs, outputs=[mos_output, distortion_output])
-    return model
-
-tid2013_loader = tid2013_loader(download=False)
-train, val, test = tid2013_loader.train, tid2013_loader.val, tid2013_loader.test
-base_dir = os.path.join(os.getcwd(), 'databases/tid2013/normalized_distorted_images')
-train_generator, val_generator, test_generator = create_data_generators(base_dir, train, val, test, batch_size=32)
-
-
-model = define_model()
-model.compile(optimizer='adam',
-              loss={'dmos': 'mae', 'distortion': 'categorical_crossentropy'},
-              metrics={'dmos': 'mae', 'distortion': 'accuracy'})
-
-history = model.fit(train_generator, epochs=25, validation_data=val_generator, verbose=2)
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+history = model.fit(X_train, y_train, epochs=5, batch_size=32, validation_data=(X_val, y_val))
+loss, mae = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', loss)
+print('Test MAE:', mae)
