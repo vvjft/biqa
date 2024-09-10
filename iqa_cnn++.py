e018de64be9bb455ce6c136008284a3b469024ec
Wojciech Trojanowski <wjf.trojanowski@gmail.com>
2024-09-10 03:02:52 +0200

    Small refine; new terminal arguments added

--- iqa_cnn++.py
+++ iqa_cnn++.py
@@ -11,11 +11,8 @@ from scipy.stats import spearmanr, pearsonr, kendalltau
 from scipy.optimize import curve_fit
 
 # TO DO:
-    # save results to txt
-    # check if mapping is correct
-    # create model without classification
+    # verify mos2dmos mapping
     
-
 def split_data(metadata, measureName, validation=True):  
     metadata = metadata.reset_index().rename(columns={'index': 'original_index'})
     metadata['image_id'] = metadata['image'].apply(lambda x: '_'.join(x.split('_')[:4]))
@@ -85,7 +82,7 @@ def build_model2(): # no classification
     ])
     return model
     
-def show_results(meta_test, y_pred_reg, y_pred_class, measureName, distortion_mapping):
+def show_results(meta_test, y_pred_reg, y_pred_class, measureName, distortion_mapping, classify=True):
     def group_results(metadata, measureName):
         metadata['prefix'] = metadata['image'].str.extract(r'(i\d+_\d+_\d+)_patch')
 
@@ -93,7 +90,7 @@ def show_results(meta_test, y_pred_reg, y_pred_class, measureName, distortion_ma
             measure=(measureName, 'first'),  
             pred_measure=(f'pred_{measureName}', 'mean'),
             distortion=('distortion', 'first'),
-            pred_distortion = ('pred_distortion', lambda x: x.mode().iloc[0])  
+            pred_distortion = ('pred_distortion', lambda x: x.mode().iloc[0] if x.notna().any() else None)  
         ).reset_index()
 
         grouped.rename(columns={'prefix': 'image', 'measure': measureName, 'pred_measure': f'pred_{measureName}'}, inplace=True)
@@ -102,86 +99,55 @@ def show_results(meta_test, y_pred_reg, y_pred_class, measureName, distortion_ma
     sequential_mapping = {i: key for i, key in enumerate(sorted(distortion_mapping.keys()))}
     
     meta_test[measureName] = pd.to_numeric(meta_test[measureName], errors='coerce').astype('float32')
-    meta_test['distortion'] = pd.to_numeric(meta_test['distortion'], errors='coerce').astype('int64')
-    indices = np.argmax(y_pred_class, axis=1)
     meta_test[f'pred_{measureName}'] = y_pred_reg.flatten()
-    meta_test['pred_distortion'] = indices
-    meta_test['distortion'] = meta_test['distortion'].map(sequential_mapping).map(distortion_mapping)
-    meta_test['pred_distortion'] = meta_test['pred_distortion'].map(sequential_mapping).map(distortion_mapping)
-    meta_test.to_csv('meta_test.csv')
+    meta_test['pred_distortion'] = None
+    if classify:
+        meta_test['distortion'] = pd.to_numeric(meta_test['distortion'], errors='coerce').astype('int64')
+        indices = np.argmax(y_pred_class, axis=1)      
+        meta_test['pred_distortion'] = indices
+        meta_test['distortion'] = meta_test['distortion'].map(sequential_mapping).map(distortion_mapping)
+        meta_test['pred_distortion'] = meta_test['pred_distortion'].map(sequential_mapping).map(distortion_mapping)
+    #meta_test.to_csv('meta_test.csv')
     results = group_results(meta_test, measureName)
     results.to_csv('results.csv')
 
-    acc = accuracy_score(results['distortion'], results['pred_distortion'])
     lcc = pearsonr(results[f'pred_{measureName}'], results[measureName])[0]
     srocc = spearmanr(results[f'pred_{measureName}'], results[measureName])[0]
     krcc = kendalltau(results[f'pred_{measureName}'], results[measureName])[0]
     mae = mean_absolute_error(results[measureName], results[f'pred_{measureName}'])
+    if classify:
+        acc = accuracy_score(results['distortion'], results['pred_distortion'])
     
     with open('results.txt', 'w') as file:
         file.write('All:\n')
-        file.write(f'  ACC (Accuracy): {acc}\n')
+        
         file.write(f'  LCC (Linear Correlation Coefficient): {lcc}\n')
         file.write(f'  SROCC (Spearman Rank Order Correlation Coefficient): {srocc}\n')
         file.write(f'  KRCC (Kendall Rank Correlation Coefficient): {krcc}\n')
         file.write(f'  MAE (Mean Absolute Error): {mae}\n')
-        
-        distortions = results.groupby('distortion') 
-        for name, distortion in distortions:
-            group_accuracy_score = accuracy_score(distortion['distortion'], distortion['pred_distortion'])
-            group_lcc = pearsonr(distortion[f'pred_{measureName}'], distortion[measureName])[0]
-            group_srocc = spearmanr(distortion[f'pred_{measureName}'], distortion[measureName])[0]
-            group_krcc = kendalltau(distortion[f'pred_{measureName}'], distortion[measureName])[0]
-            group_mae = mean_absolute_error(distortion[measureName], distortion[f'pred_{measureName}'])
-        
-            file.write(f'{name}:\n')
-            file.write(f'  ACC: {group_accuracy_score}\n')
-            file.write(f'  LCC: {group_lcc}\n')
-            file.write(f'  SROCC: {group_srocc}\n')
-            file.write(f'  KRCC: {group_krcc}\n')
-            file.write(f'  MAE: {group_mae}\n')
+        if classify:
+            file.write(f'  ACC (Accuracy): {acc}\n')
+            distortions = results.groupby('distortion') 
+            for name, distortion in distortions:
+                
+                group_accuracy_score = accuracy_score(distortion['distortion'], distortion['pred_distortion'])
+                group_lcc = pearsonr(distortion[f'pred_{measureName}'], distortion[measureName])[0]
+                group_srocc = spearmanr(distortion[f'pred_{measureName}'], distortion[measureName])[0]
+                group_krcc = kendalltau(distortion[f'pred_{measureName}'], distortion[measureName])[0]
+                group_mae = mean_absolute_error(distortion[measureName], distortion[f'pred_{measureName}'])
+            
+                file.write(f'{name}:\n')
+                file.write(f'  ACC: {group_accuracy_score}\n')
+                file.write(f'  LCC: {group_lcc}\n')
+                file.write(f'  SROCC: {group_srocc}\n')
+                file.write(f'  KRCC: {group_krcc}\n')
+                file.write(f'  MAE: {group_mae}\n')
     print(f'Results saved to results.txt')
 
-
-
-def show_results2(meta_test, y_pred_reg, measureName):
-    def group_results(metadata, measureName):
-        metadata['prefix'] = metadata['image'].str.extract(r'(i\d+_\d+_\d+)_patch')
-
-        grouped = metadata.groupby('prefix').agg(
-            measure=(measureName, 'first'),  
-            pred_measure=(f'pred_{measureName}', 'mean') 
-        ).reset_index()
-
-        grouped.rename(columns={'prefix': 'image', 'measure': measureName, 'pred_measure': f'pred_{measureName}'}, inplace=True)
-        return grouped
-    
-    meta_test[measureName] = pd.to_numeric(meta_test[measureName], errors='coerce').astype('float32')
-    meta_test[f'pred_{measureName}'] = y_pred_reg.flatten()
-    meta_test.to_csv('meta_test.csv')
-    results = group_results(meta_test, measureName)
-    results.to_csv('results.csv')
-
-    
-    lcc = pearsonr(results[f'pred_{measureName}'], results[measureName])[0]
-    srocc = spearmanr(results[f'pred_{measureName}'], results[measureName])[0]
-    krcc = kendalltau(results[f'pred_{measureName}'], results[measureName])[0]
-    mae = mean_absolute_error(results[measureName], results[f'pred_{measureName}'])
-    
-    with open('results.txt', 'w') as file:
-        file.write('All:\n')
-        file.write(f'  LCC (Linear Correlation Coefficient): {lcc}\n')
-        file.write(f'  SROCC (Spearman Rank Order Correlation Coefficient): {srocc}\n')
-        file.write(f'  KRCC (Kendall Rank Correlation Coefficient): {krcc}\n')
-        file.write(f'  MAE (Mean Absolute Error): {mae}\n')
-        
-    print(f'Results saved to results.txt')
-
-def main(training, test):
+def main(training, test, epochs, classify=True):
     databases = {'tid2013': tid2013_loader, 'kadid10k': kadid10k_loader}
-    
-    #training_loader = databases[training](test, as_training=True)
-    training_loader = databases[training](training, as_training=False)
+    training_loader = databases[training](test, as_training=True)
+    #training_loader = databases[training](training, as_training=False)
     num_classes = training_loader.num_classes
     training_data = training_loader.metadata
     X = training_loader.X
@@ -206,7 +172,7 @@ def main(training, test):
         
         training_data[training_measureName] = mos2dmos(training_data[training_measureName], test_data[test_measureName])
         meta_train, meta_val = split_data(training_data, training_measureName, validation=False)
-        meta_train.to_csv('meta_train.csv')
+        #meta_train.to_csv('meta_train.csv')
         meta_test = test_loader.metadata
         train_indices, val_indices = meta_train.index, meta_val.index
         
@@ -217,19 +183,21 @@ def main(training, test):
     X_train, X_val = X[train_indices], X[val_indices] 
     y_train_reg, y_val_reg = y_reg[train_indices], y_reg[val_indices]
     y_train_class, y_val_class = y_class[train_indices], y_class[val_indices]
-            
-    #model = build_model(num_classes)
-    #model.compile(optimizer='adam', loss=['mae', 'sparse_categorical_crossentropy'])
-    #model.fit(X_train, [y_train_reg,  y_train_class], epochs=1, batch_size=32, verbose=2)
 
-    model = build_model2()
-    model.compile(optimizer='adam', loss='mean_absolute_error')
-    model.fit(X_train, y_train_reg, epochs=1, batch_size=32, verbose=2)
-
-    #y_pred_reg, y_pred_class = model.predict(X_test, verbose=2)
-    y_pred_reg = model.predict(X_test, verbose=2)
-    #show_results(meta_test, y_pred_reg, y_pred_class, measureName, distortion_mapping)
-    show_results2(meta_test, y_pred_reg,  measureName)
+    if classify:    
+        model = build_model(num_classes)
+        model.compile(optimizer='adam', loss=['mae', 'sparse_categorical_crossentropy'])
+        model.fit(X_train, [y_train_reg,  y_train_class], epochs=epochs, batch_size=32, verbose=2)
+        y_pred_reg, y_pred_class = model.predict(X_test, verbose=2)
+    else:
+        model = build_model2()
+        model.compile(optimizer='adam', loss='mean_absolute_error')
+        model.fit(X_train, y_train_reg, epochs=epochs, batch_size=32, verbose=2)
+        y_pred_reg = model.predict(X_test, verbose=2)
+        y_pred_class = None
+  
+    show_results(meta_test, y_pred_reg, y_pred_class, measureName, distortion_mapping, classify)
+    #show_results2(meta_test, y_pred_reg,  measureName)
 
 if __name__ == "__main__":
     import tensorflow as tf
@@ -243,8 +211,10 @@ if __name__ == "__main__":
     
     parser.add_argument('--training', choices=choices, type=str, default='tid2013', help='Database for training set (default: tid2013)')
     parser.add_argument('--test', choices=choices, type=str, help='Database for test set. If not specified - evaluation is done on trainig set.')
+    parser.add_argument('--epochs', type=int, default = 1, help='Number of epochs (default: 1)')
+    parser.add_argument('--no_classify', action='store_false', help='Disable classification of the distortion')
     args = parser.parse_args()
     if args.test == None:
         args.test = args.training
-    main(args.training, args.test)
+    main(args.training, args.test, args.epochs, args.no_classify)
 
